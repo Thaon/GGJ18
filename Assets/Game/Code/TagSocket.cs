97dea3fd379ba23fd2ba1f14ac8fd62aec24e802
YAP! <GMADDA200@caledonian.ac.uk>
2018-01-27 17:05:51 +0000

    added tag socket + moving parts to box

---
+++ Assets/Game/Code/TagSocket.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TagSocket : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "TAG")
+        {
+            GetComponent<MeshRenderer>().sharedMaterial.color = Color.yellow;
+            Destroy(other.gameObject);
+            transform.parent.GetComponent<BoxCustomizer>().m_hasTag = true;
+        }
+    }
+}
